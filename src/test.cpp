09b9bf5d1cb49e39aeaabe7b1d50a460a0d2949e
Mylenda Darci <karpov.vlg@gmail.com>
2025-07-29 21:05:36 +0300

    added copy, =, fixed print, upgrade style classes

--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,19 @@
+#include <cstring>
 #include <iostream>
 #include <stdexcept>
 
 class S21Matrix {
  private:
-  int rows_{0}, cols_{0};
-  double** matrix_ = NULL;
+  int rows_, cols_;
+  double** matrix_;
 
  public:
-  S21Matrix() {
-    this->rows_ = 3;
-    this->cols_ = 3;
+  S21Matrix() : rows_{3}, cols_{3} {
     this->matrix_ = new double*[this->rows_ * this->cols_]();
     for (int i = 0; i < this->rows_; i++) {
       matrix_[i] = new double[this->cols_]{0};
     }
-  };
+  }
 
   S21Matrix(int rows) {
     this->rows_ = rows;
@@ -24,7 +23,7 @@ class S21Matrix {
     for (int i = 0; i < rows; i++) {
       matrix_[i] = new double[rows]{0};
     }
-  };
+  }
 
   S21Matrix(int rows, int cols) {
     this->rows_ = rows;
@@ -32,17 +31,29 @@ class S21Matrix {
     // this->matrix_ = new double*[rows * rows]();
     this->matrix_ = new double*[rows];
     for (int i = 0; i < rows; i++) {
-      matrix_[i] = new double[cols]{0};
+      this->matrix_[i] = new double[cols]{0};
     }
-  };
+  }
 
   ~S21Matrix() { delete[] matrix_; }
+
+  S21Matrix(const S21Matrix& other) : rows_(other.rows_), cols_(other.cols_) {
+    this->matrix_ = new double*[other.rows_];
+    for (int i = 0; i < other.rows_; i++) {
+      matrix_[i] = new double[this->cols_]{0};
+      std::memcpy(this->matrix_[i], other.matrix_[i],
+                  other.cols_ * sizeof(double));
+      // std::cerr << "!" << *this->matrix_[i] << "!" << " !" <<
+      // *other.matrix_[i]
+      // << "!" << std::endl;
+    }
+  }
 
   void SumMatrix(const S21Matrix& other) {
     if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
       throw std::exception();
 
-    } else if (this->matrix_ == NULL || other.matrix_ == NULL) {
+    } else if (this->matrix_ == nullptr || other.matrix_ == nullptr) {
       throw std::exception();
     }
     S21Matrix(this->rows_, this->cols_);
@@ -53,18 +64,26 @@ class S21Matrix {
     }
   }
 
-  // S21Matrix S21Matrix::operator+(const S21Matrix& other) {
-  //   // creating result matrix
-  //   S21Matrix res(this->rows_, this->cols_);
-  //   res.SumMatrix(other);
-  //   return res;
-  // }
+  void operator+(const S21Matrix& other) {
+    // creating result matrix
+
+    this->SumMatrix(other);
+  }
+
+  const S21Matrix& operator=(const S21Matrix& other) {
+    for (int i = 0; i < this->rows_; i++) {
+      delete[] this->matrix_[i];
+    }
+    delete[] this->matrix_;
+    *this = other;
+    return other;
+  }
 
   void SubMatrix(const S21Matrix& other) {
     if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
       throw std::exception();
 
-    } else if (this->matrix_ == NULL || other.matrix_ == NULL) {
+    } else if (this->matrix_ == nullptr || other.matrix_ == nullptr) {
       throw std::exception();
     }
     S21Matrix(this->rows_, this->cols_);
@@ -79,7 +98,7 @@ class S21Matrix {
     if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
       throw std::exception();
 
-    } else if (this->matrix_ == NULL || other.matrix_ == NULL) {
+    } else if (this->matrix_ == nullptr || other.matrix_ == nullptr) {
       throw std::exception();
     }
     S21Matrix(this->rows_, this->cols_);
@@ -90,6 +109,21 @@ class S21Matrix {
     }
   }
 
+  // S21Matrix::S21Matrix(S21Matrix&& other) {
+  //   if (rows_ * cols_ == other.rows_ * other.cols_) {
+  //     std::memcpy(p_, other.p_, other.cols_ * other.rows_ * sizeof(double));
+  //   } else {
+  //     delete[] p_;
+  //     p_ = new double[other.rows_ * other.cols_]();
+  //     std::memcpy(p_, other.p_, other.cols_ * other.rows_ * sizeof(double));
+  //   }
+  //   rows_ = other.rows_;
+  //   cols_ = other.cols_;
+  //   delete other.p_;
+  //   other.rows_ = 0;
+  //   other.cols_ = 0;
+  // }
+
   int getRows() { return this->rows_; }
 
   int getCols() { return this->cols_; }
@@ -98,7 +132,7 @@ class S21Matrix {
 
   void setCols(int cols) { this->cols_ = cols; }
 
-  // double** setMatrix(int rows, int cols, double number,
+  // double** setNumToMatrix(int rows, int cols, double number,
   //                    const S21Matrix& other) {
   //   for (int i = 0; i < rows; i++) {
   //     for (int j = 0; j < cols; j++) {
@@ -107,7 +141,7 @@ class S21Matrix {
   //   }
   // }
 
-  double getMatrix(int row, int col) {
+  double getMatrix(int row, int col) const {
     // for (int i = 0; i < this->rows_; i++) {
     //   for (int j = 0; j < this->cols_; j++) {
     return this->matrix_[row][col];
@@ -115,7 +149,7 @@ class S21Matrix {
     // }
   }
 
-  void setMatrix(int row, int col, double number) {
+  void setNumToMatrix(int row, int col, double number) {
     // for (int i = 0; i < this->rows_; i++) {
     //   for (int j = 0; j < this->cols_; j++) {
     this->matrix_[row][col] = number;
@@ -126,7 +160,12 @@ class S21Matrix {
   void printMatr() {
     for (int i = 0; i < this->rows_; i++) {
       for (int j = 0; j < this->cols_; j++) {
-        std::cout << this->matrix_[i][j] << std::endl;
+        if (i == this->rows_ - 1 && j == this->cols_ - 1)
+          std::cout << this->matrix_[i][j];
+        else if (j == this->cols_ - 1)
+          std::cout << this->matrix_[i][j] << std::endl;
+        else
+          std::cout << this->matrix_[i][j] << " ";
       }
     }
   }
@@ -137,20 +176,27 @@ int main() {
 
   {
     S21Matrix* pM = new S21Matrix(4, 5);
-    S21Matrix* pM2 = new S21Matrix(4);  //
+
+    S21Matrix* pM2 = new S21Matrix(4, 5);  //
     // S21Matrix* pMres = new S21Matrix(4, 5);
     // pM->SumMatrix->matrix_[0][0] = 1.0;
-    pM2->setMatrix(0, 0, 1);
+    pM2->setNumToMatrix(0, 0, 1.0);
     pM->SumMatrix(*pM2);
     std::cout << pM->getMatrix(0, 0) << std::endl;
+    S21Matrix pMres{*pM};  // copy
+    std::cout << pMres.getMatrix(0, 0) << std::endl;
     pM->SubMatrix(*pM2);
     std::cout << pM->getMatrix(0, 0) << std::endl;
-    // pM->printMatr();
+    S21Matrix* pM21 = pM2;
+    std::cout << pM21->getMatrix(0, 0) << std::endl;
+    pM21->printMatr();
+
     delete pM;
     delete pM2;
   } catch (const std::exception& err) {
     std::cerr << "[exception] " << err.what() << std::endl;
   }
+
   return 0;
 
   // delete pMres;
